bd685df85c9461c3567cc7d9a424194c75bfc7f6
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-11-17 20:42:11 -0300

    add Amplify app from aws

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "appreceitas7fde1711": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
