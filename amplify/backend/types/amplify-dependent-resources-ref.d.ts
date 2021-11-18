1b54b945fd26db4693e73adec23dc6bb424a4717
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-11-17 21:19:01 -0300

    add graphql api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "receitasAPI": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
