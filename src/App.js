e9b95c96816ecf57b0537193f10f21d50f28d1e1
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-11-22 22:18:01 -0300

    add scr app and index | update package locks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <h1>É isso time</h1>
+        <p>Agora com Auth:</p>
+        <AmplifySignOut />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
