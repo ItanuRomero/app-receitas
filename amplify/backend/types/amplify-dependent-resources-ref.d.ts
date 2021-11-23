8309924869b0c82ec57994d49128651624f5ffbf
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-11-22 21:51:05 -0300

    add amplify auth and save lockfile

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "appreceitas9551df39": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
