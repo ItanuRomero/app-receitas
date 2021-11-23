9957b5a23feba7da1cb2df5fb4f70c8838511d64
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-11-22 22:57:02 -0300

    add api schema model

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateRecipe = /* GraphQL */ `
+  subscription OnCreateRecipe {
+    onCreateRecipe {
+      id
+      title
+      description
+      ingredients
+      portions
+      preparationMethod
+      tips
+      preparationTime
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateRecipe = /* GraphQL */ `
+  subscription OnUpdateRecipe {
+    onUpdateRecipe {
+      id
+      title
+      description
+      ingredients
+      portions
+      preparationMethod
+      tips
+      preparationTime
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteRecipe = /* GraphQL */ `
+  subscription OnDeleteRecipe {
+    onDeleteRecipe {
+      id
+      title
+      description
+      ingredients
+      portions
+      preparationMethod
+      tips
+      preparationTime
+      createdAt
+      updatedAt
+    }
+  }
+`;
