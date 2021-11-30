8963beca7afc0c4a5c1acce81b12383d4b49a870
Gabriel Gigante <gabrielgigante29@gmail.com>
2021-11-30 20:36:23 -0300

    adjusting entry component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-import { I18n } from 'aws-amplify';
+import Amplify, { I18n } from 'aws-amplify';
 import AmplifyI18n from 'amplify-i18n';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import NewRecipe from './routes/newRecipe';
-import ListRecipes from './routes/listRecipes';
+import App from './App';
+import config from './aws-exports';
+
+const locales = ["en", "fr", "pt-BR"];
 
 Amplify.configure(config);
-const locales = ["en", "fr", "pt-BR"];
 AmplifyI18n.configure(locales);
+
 I18n.setLanguage("pt-BR");
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="new" element={<NewRecipe />} />
-      <Route path="recipes" element={<ListRecipes />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
