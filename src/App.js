e4cb27025c8023bb0ce25ec20548dabfb80d433c
Gabriel Gigante <gabrielgigante29@gmail.com>
2021-11-30 20:58:49 -0300

    adjusting app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import { Link } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { AppRoutes } from './routes';
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+import './styles/global.css';
 
 function App() {
   return (
-      <div className="App">
-        <Header/>
-          <main>
-              <Link to="/recipes">
-                  <h1>Ver receitas</h1>
-              </Link>
-              <Link to="/new">
-                  <h1>Criar receitas</h1>
-              </Link>
-          </main>
-        <Footer/>
-      </div>
+    <Router>
+      <Header/>
+      
+      <main>
+        <AppRoutes />
+      </main>
+      
+      <Footer/>
+    </Router>
   );
 }
 
